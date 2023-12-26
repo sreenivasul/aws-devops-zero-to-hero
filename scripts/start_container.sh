141bdae6aed9b52ba0a848b78b22e40dd8555359
Sreenivasulu Gali <89188614+sreenivasul@users.noreply.github.com>
2023-12-26 16:37:44 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull srinivasulugali/simple-python-flask-app 
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 srinivasulugali/simple-python-flask-app 
+docker run -d -p 5000:5000 srinivasulugali/simple-python-flask-app  
